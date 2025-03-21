32b2fb0a8931075549dc3b0c799f0bd41397f8f7
dudu4leal <eduardoleal.ferreir4@gmail.com>
2025-03-21 20:13:20 -0300

    adicionando exercício 43

---
+++ ex043.py
@@ -0,0 +1,22 @@
+#programa que lê peso(kg) e altura(m) de uma pessoa, calcula seu IMC e diz a situação do seu índice de massa corporal
+
+peso = float(input('peso em Kg: '))
+altura = float (input('altura em metros: '))
+
+imc = peso / altura**2
+#print(imc)
+
+if imc < 18.5:
+    print('imc = {:.1f}\nabaixo do peso'.format(imc))
+
+elif imc >= 18.5 and imc <= 25:
+    print('imc = {:.1f}\npeso ideal'.format(imc))
+
+elif imc > 25 and imc <= 30:
+    print('imc = {:.1f}\nsobrepeso')
+
+elif imc > 30 and imc <= 40:
+    print('imc = {:.1f}\nobesidade')
+
+elif imc > 40:
+    print('imc = {:.1f}\nobesidade mórbida')
